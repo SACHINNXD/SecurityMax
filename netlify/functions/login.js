d0a06099ea1608e352d8c4c163ae10bc0dc54a52
SACHINNXD <145748982+SACHINNXD@users.noreply.github.com>
2026-01-07 13:46:45 +0530

    Create login.js

---
+++ netlify/functions/login.js
@@ -0,0 +1,15 @@
+export async function handler() {
+    const params = new URLSearchParams({
+        client_id: process.env.DISCORD_CLIENT_ID,
+        redirect_uri: process.env.DISCORD_REDIRECT_URI,
+        response_type: "code",
+        scope: "identify email"
+    });
+
+    return {
+        statusCode: 302,
+        headers: {
+            Location: `https://discord.com/oauth2/authorize?${params}`
+        }
+    };
+}
