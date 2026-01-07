95fa71181d7b95d27dedd4855ba6cab429e144f0
SACHINNXD <145748982+SACHINNXD@users.noreply.github.com>
2026-01-07 20:56:16 +0530

    Update logout.js

--- netlify/functions/logout.js
+++ netlify/functions/logout.js
@@ -1,9 +1,10 @@
 export async function handler() {
-    return {
-        statusCode: 302,
-        headers: {
-            "Set-Cookie": "user=; Path=/; Max-Age=0",
-            Location: "/"
-        }
-    };
+  return {
+    statusCode: 302,
+    headers: {
+      "Set-Cookie":
+        "user=; Path=/; Max-Age=0; HttpOnly; SameSite=Lax; Secure",
+      Location: "/"
+    }
+  };
 }
